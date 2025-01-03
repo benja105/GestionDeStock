f263c5ac708a4407413b33a556cd3bf1249f5e36
benja105 <98060364+benja105@users.noreply.github.com>
2025-01-03 20:26:00 -0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -21,10 +21,7 @@ app.get("/", (req, res) => {
 });
 
 // ** Conexión a MongoDB **
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-});
+mongoose.connect(process.env.MONGO_URI)
 
 mongoose.connection.on("error", (err) => {
   console.error("Error al conectar a MongoDB:", err);
